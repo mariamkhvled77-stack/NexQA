2f276df3f77dd40f8631da70eb415f7c29833c7d
mariamkhvled77-stack <mariamkhvled77@gmail.com>
2026-08-10 23:05:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -827,6 +827,7 @@ def api_color_history():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     port = int(os.getenv('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
-
